0c57f123d1e40822c369ee2e2f9f1c68c9ee2b2f
smpetrov <smpetrov@yahoo.com>
2017-01-23 17:57:13 +0200

    work on 230117

---
+++ MS/BlogDB/MVC-Blog/MVC-Blog/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Blog.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ MS/BlogDB/MVC-Blog/MVC-Blog/Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Blog.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public String Title { get; set; }
+
+        [Required]
+        public String Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+    }
+}
