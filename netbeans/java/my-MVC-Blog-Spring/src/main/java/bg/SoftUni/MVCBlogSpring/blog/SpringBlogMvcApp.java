4b157a5f00086bdf015bbf7cf72994da36154c2b
smpetrov <smpetrov@yahoo.com>
2017-02-08 16:42:38 +0200

    work on 080217

---
+++ netbeans/java/my-MVC-Blog-Spring/src/main/java/bg/SoftUni/MVCBlogSpring/blog/SpringBlogMvcApp.java
@@ -0,0 +1,12 @@
+
+package bg.SoftUni.MVCBlogSpring.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogMvcApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogMvcApp.class, args);
+    }
+}
